ff7c47641288c3fe38fa052d744ebdb2b58f3fee
Anatolii Davydko <anatoly.davydko@gmail.com>
2019-11-29 09:03:18 +0200

    Special commit: STOMP protocol is used.

--- TriciyenServer/src/main/java/root/configuration/WebSocketConfiguration.java
+++ TriciyenServer/src/main/java/root/configuration/WebSocketConfiguration.java
@@ -9,12 +9,28 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+
+    private static final String broker1 = "/broker1";
+    private static final String broker2 = "/broker2";
+    private static final String broker3 = "/broker3";
+    private static final String brokerPrefix = "/app_pref";
+    private static final String stompEndPoint1 = "/endpoint1";
+    private static final String stompEndPoint2 = "/endpoint2";
+    private static final String stompEndPoint3 = "/endpoint3";
+
+
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/chat");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker(broker1);
+        config.enableSimpleBroker(broker2);
+        config.enableSimpleBroker(broker3);
+        config.setApplicationDestinationPrefixes(brokerPrefix);
     }
 
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat-messaging").withSockJS();
+        registry.addEndpoint(stompEndPoint1).withSockJS();
+        registry.addEndpoint(stompEndPoint2).withSockJS();
+        registry.addEndpoint(stompEndPoint3).withSockJS();
     }
 }
--- TriciyenServer/src/main/java/root/controllers/UserAccountController.java
+++ TriciyenServer/src/main/java/root/controllers/UserAccountController.java
@@ -1,31 +1,44 @@
 package root.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import root.entity.UserAccount;
-import root.repository.UserAccountRepository;
+import root.entity.Greeting;
+import root.entity.HelloMessage;
 
 @Controller
-@RequestMapping(path="/demo")
 public class UserAccountController {
-    @Autowired
-    private UserAccountRepository userAccountRepository;
+    private static final String stompEndPoint1 = "/endpoint1";
+    private static final String stompEndPoint2 = "/endpoint2";
+    private static final String stompEndPoint3 = "/endpoint3";
+    private static final String broker1 = "/broker1";
+    private static final String broker2 = "/broker2";
+    private static final String broker3 = "/broker3";
+    private static final String topic1 = "/topic1";
+    private static final String topic2 = "/topic2";
+    private static final String topic3 = "/topic3";
 
-    @PostMapping(path="/add")
-    public @ResponseBody String addNewUserAccount (
-            @RequestParam String login,
-            @RequestParam String password,
-            @RequestParam String name,
-            @RequestParam String email) {
-
-        UserAccount userAccount = new UserAccount(login, password, name, email);
-        userAccountRepository.save(userAccount);
-        return "Saved";
+    @MessageMapping(stompEndPoint1)
+    @SendTo(broker1 + topic1)
+    public Greeting greeting1(HelloMessage message) throws Exception {
+        System.out.println("Sending Greeting1");
+        Thread.sleep(1000);
+        return new Greeting("Hello1, " + message.getName() + "!");
     }
 
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<UserAccount> getAllUsers() {
-        return userAccountRepository.findAll();
+    @MessageMapping(stompEndPoint2)
+    @SendTo(broker2 + topic2)
+    public Greeting greeting2(HelloMessage message) throws Exception {
+        System.out.println("Sending Greeting2");
+        Thread.sleep(1000);
+        return new Greeting("Hello2, " + message.getName() + "!");
+    }
+
+    @MessageMapping(stompEndPoint3)
+    @SendTo(broker3 + topic3)
+    public Greeting greeting3(HelloMessage message) throws Exception {
+        System.out.println("Sending Greeting3");
+        Thread.sleep(1000);
+        return new Greeting("Hello3, " + message.getName() + "!");
     }
 }
