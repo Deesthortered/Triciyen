9493a5666e78a1974122b1b217092fb695823b7e
Anatolii Davydko <anatoly.davydko@gmail.com>
2019-11-30 15:11:43 +0200

    Working STOMP.

--- TriciyenClient/src/main/java/com/triciyen/TriciyenApplication.java
+++ TriciyenClient/src/main/java/com/triciyen/TriciyenApplication.java
@@ -21,36 +21,22 @@ public class TriciyenApplication extends Application implements EventHandler<Eve
     private static final String host = "localhost";
     private static final String port = "8080";
     private static final String stompEndPoint1 = "/endpoint1";
-    private static final String stompEndPoint2 = "/endpoint2";
-    private static final String stompEndPoint3 = "/endpoint3";
     private static final String broker1 = "/broker1";
-    private static final String broker2 = "/broker2";
-    private static final String broker3 = "/broker3";
     private static final String topic1 = "/topic1";
-    private static final String topic2 = "/topic2";
-    private static final String topic3 = "/topic3";
     private static final String brokerPrefix = "/app_pref";
 
     private StompConnector connector1 = new StompConnector();
-    private StompConnector connector2 = new StompConnector();
-    private StompConnector connector3 = new StompConnector();
 
     private Button button1;
-    private Button button2;
-    private Button button3;
 
     @Override
     public void start(Stage stage) {
         button1 = new Button("Button1 caption");
-        button2 = new Button("Button2 caption");
-        button3 = new Button("Button3 caption");
 
         button1.setOnMouseClicked(this);
-        button2.setOnMouseClicked(this);
-        button3.setOnMouseClicked(this);
 
         Pane pane = new VBox();
-        pane.getChildren().addAll(button1, button2, button3);
+        pane.getChildren().addAll(button1);
         Scene scene = new Scene(pane, 300, 300);
         stage.setScene(scene);
         stage.setTitle("JavaFX and Gradle");
@@ -73,13 +59,7 @@ public class TriciyenApplication extends Application implements EventHandler<Eve
         if (event.getSource() == button1) {
             System.out.println("Button1 pressed");
             connector1.send(brokerPrefix + stompEndPoint1, "{ \"name\" : \"dude1!\"}");
-        } else if (event.getSource() == button2) {
-            System.out.println("Button2 pressed");
-            connector1.send(brokerPrefix + stompEndPoint2, "{ \"name\" : \"dude2!\"}");
-        } else if (event.getSource() == button3) {
-            System.out.println("Button3 pressed");
-            connector1.send(brokerPrefix + stompEndPoint2, "{ \"name\" : \"dude3!\"}");
-        } else  {
+        } else {
             System.out.println("Another event");
         }
     }
@@ -92,20 +72,6 @@ public class TriciyenApplication extends Application implements EventHandler<Eve
                 System.out.println("Connected1!");
             }
         });
-        connector2.connect(host, port, stompEndPoint2, new StompSessionHandlerAdapter() {
-            @Override
-            public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-                System.out.println("Connected2!");
-            }
-        });
-        connector3.connect(host, port, stompEndPoint2, new StompSessionHandlerAdapter() {
-            @Override
-            public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-                System.out.println("Connected3!");
-            }
-        });
-
-
 
         connector1.subscribe(broker1 + topic1, new StompFrameHandler() {
             @Override
@@ -118,31 +84,6 @@ public class TriciyenApplication extends Application implements EventHandler<Eve
                 System.out.println("1 Received greeting " + new String((byte[]) o));
             }
         });
-
-        connector2.subscribe(broker1 + topic1, new StompFrameHandler() {
-            @Override
-            public Type getPayloadType(StompHeaders stompHeaders) {
-                return byte[].class;
-            }
-
-            @Override
-            public void handleFrame(StompHeaders stompHeaders, Object o) {
-                System.out.println("2 Received greeting " + new String((byte[]) o));
-            }
-        });
-
-        connector3.subscribe(broker1 + topic1, new StompFrameHandler() {
-            @Override
-            public Type getPayloadType(StompHeaders stompHeaders) {
-                return byte[].class;
-            }
-
-            @Override
-            public void handleFrame(StompHeaders stompHeaders, Object o) {
-                System.out.println("3 Received greeting " + new String((byte[]) o));
-            }
-        });
-
     }
 }
 
