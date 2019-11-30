c706c88ba5e377affa74006a777f4ab1ed5dca1f
Anatolii Davydko <anatoly.davydko@gmail.com>
2019-11-30 15:11:24 +0200

    Working STOMP.

--- TriciyenServer/src/main/java/root/configuration/WebSocketConfiguration.java
+++ TriciyenServer/src/main/java/root/configuration/WebSocketConfiguration.java
@@ -11,26 +11,18 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
 
     private static final String broker1 = "/broker1";
-    private static final String broker2 = "/broker2";
-    private static final String broker3 = "/broker3";
     private static final String brokerPrefix = "/app_pref";
     private static final String stompEndPoint1 = "/endpoint1";
-    private static final String stompEndPoint2 = "/endpoint2";
-    private static final String stompEndPoint3 = "/endpoint3";
 
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker(broker1);
-        config.enableSimpleBroker(broker2);
-        config.enableSimpleBroker(broker3);
         config.setApplicationDestinationPrefixes(brokerPrefix);
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint(stompEndPoint1).withSockJS();
-        registry.addEndpoint(stompEndPoint2).withSockJS();
-        registry.addEndpoint(stompEndPoint3).withSockJS();
     }
 }
--- TriciyenServer/src/main/java/root/controllers/UserAccountController.java
+++ TriciyenServer/src/main/java/root/controllers/UserAccountController.java
@@ -9,36 +9,14 @@ import root.entity.HelloMessage;
 @Controller
 public class UserAccountController {
     private static final String stompEndPoint1 = "/endpoint1";
-    private static final String stompEndPoint2 = "/endpoint2";
-    private static final String stompEndPoint3 = "/endpoint3";
     private static final String broker1 = "/broker1";
-    private static final String broker2 = "/broker2";
-    private static final String broker3 = "/broker3";
     private static final String topic1 = "/topic1";
-    private static final String topic2 = "/topic2";
-    private static final String topic3 = "/topic3";
 
     @MessageMapping(stompEndPoint1)
-    @SendTo(broker1 + topic1)
+    @SendTo((broker1 + topic1))
     public Greeting greeting1(HelloMessage message) throws Exception {
         System.out.println("Sending Greeting1");
         Thread.sleep(1000);
         return new Greeting("Hello1, " + message.getName() + "!");
-    }
-
-    @MessageMapping(stompEndPoint2)
-    @SendTo(broker2 + topic2)
-    public Greeting greeting2(HelloMessage message) throws Exception {
-        System.out.println("Sending Greeting2");
-        Thread.sleep(1000);
-        return new Greeting("Hello2, " + message.getName() + "!");
-    }
-
-    @MessageMapping(stompEndPoint3)
-    @SendTo(broker3 + topic3)
-    public Greeting greeting3(HelloMessage message) throws Exception {
-        System.out.println("Sending Greeting3");
-        Thread.sleep(1000);
-        return new Greeting("Hello3, " + message.getName() + "!");
     }
 }
