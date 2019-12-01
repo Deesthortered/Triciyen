1dbe8eb4fba63bde1b5512a434e8e95e2d24b6dd
Andrii Perun <andrewfreelan@gmail.com>
2019-12-01 18:03:20 +0200

    feat: add main and connection classes

---
+++ TriciyenClient/src/main/java/com/triciyen/Messanger_Main.java
@@ -0,0 +1,27 @@
+package com.triciyen;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import com.triciyen.view.Messanger_Panel;
+
+public class Messanger_Main extends Application {
+
+    public static Messanger_Panel messangerPanel = new Messanger_Panel();
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Messanger_Panel messangerPanel = new Messanger_Panel();
+
+        messangerPanel.loginStage(primaryStage);
+        //primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
